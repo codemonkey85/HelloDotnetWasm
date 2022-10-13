e8f5bfa5adfe9af7901a0fc832b36e03dc2f4382
Michael Bond <michael@bondcodes.com>
2022-10-12 20:57:23 -0400

    More cleanup

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-import { dotnet } from './dotnet.js'
+import { dotnet } from "./dotnet.js"
 
-const is_browser = typeof window != "undefined";
-if (!is_browser) throw new Error(`Expected to be running in a browser`);
+const isBrowser = typeof window != "undefined";
+if (!isBrowser) throw new Error(`Expected to be running in a browser`);
 
 const { setModuleImports, getAssemblyExports, getConfig, runMainAndExit } = await dotnet
     .withDiagnosticTracing(false)
